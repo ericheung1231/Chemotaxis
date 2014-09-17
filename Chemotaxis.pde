7e63ac4b27b98648e59d9244bb93a1331678ebc7
unknown <ercheung3@697-334-Dale.sf.k12.internal>
2014-09-17 12:26:55 -0700

    Revised Chemotaxis

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -4,7 +4,7 @@
  {    
  	frameRate(20); 
  	size(700,700);
- 	colony = new Bacteria[500];
+ 	colony = new Bacteria[700];
  	for (int i=0; i<colony.length; i++)
  	{
  		colony[i] = new Bacteria(250,250);
@@ -21,19 +21,21 @@
  	textAlign(CENTER,CENTER);
  	textSize(15);
  	fill(0,255,0,50);
- 	ellipse(mouseX, mouseY, 50,50);
+ 	stroke(0,255,0,100);
+ 	rect(mouseX-25, mouseY-25, 50,50,20);
  	fill(0);
  	text("FOOD",mouseX, mouseY);
  }  
  class Bacteria    
  {
- 	int myX, myY, col,scal;
+ 	int myX, myY, col, scal, blah;
  	Bacteria(int x,int y)
  	{
  		myX=x;
  		myY=y;
  		col = color(0,255,255);
- 		scal = (int)(Math.random()*11);
+ 		scal = (int)(Math.random()*9)+1;
+ 		blah = (int)(Math.random()*4);
 
  	}
  	void move()
@@ -65,9 +67,28 @@
  	}
  	void show()
  	{
- 		fill(col,20);
- 		stroke(col,100);
- 		ellipse(myX, myY, scal, scal);
+ 		// fill(col,20);
+ 		// stroke(col,100);
+ 		if (blah == 0)
+ 		{
+ 			fill(col,20);
+ 			stroke(col,100);
+ 			rect(myX, myY, scal, scal);	
+ 		}
+ 		if (blah == 1)
+ 		{
+ 			fill(255,255,0,20);
+ 			stroke(255,255,0,100);
+ 			ellipse(myX, myY, scal, scal);
+ 		}
+ 		// if (blah == 2)
+ 		// {
+ 		// 	triangle(myX, myY,myX+scal/2, myY+scal, myX-scal, myY+scal/2);
+ 		// }
+ 		// if (blah == 3)
+ 		// {
+ 		// 	fill(0,0,0,20);
+ 		// }
 
  	}
  }
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -20,7 +20,7 @@ public class Chemotaxis extends PApplet {
  {    
  	frameRate(20); 
  	size(700,700);
- 	colony = new Bacteria[500];
+ 	colony = new Bacteria[700];
  	for (int i=0; i<colony.length; i++)
  	{
  		colony[i] = new Bacteria(250,250);
@@ -37,19 +37,21 @@ public class Chemotaxis extends PApplet {
  	textAlign(CENTER,CENTER);
  	textSize(15);
  	fill(0,255,0,50);
- 	ellipse(mouseX, mouseY, 50,50);
+ 	stroke(0,255,0,100);
+ 	rect(mouseX-25, mouseY-25, 50,50,15);
  	fill(0);
  	text("FOOD",mouseX, mouseY);
  }  
  class Bacteria    
  {
- 	int myX, myY, col,scal;
+ 	int myX, myY, col, scal, blah;
  	Bacteria(int x,int y)
  	{
  		myX=x;
  		myY=y;
  		col = color(0,255,255);
- 		scal = (int)(Math.random()*11);
+ 		scal = (int)(Math.random()*9)+1;
+ 		blah = (int)(Math.random()*4);
 
  	}
  	public void move()
@@ -81,9 +83,28 @@ public class Chemotaxis extends PApplet {
  	}
  	public void show()
  	{
- 		fill(col,20);
- 		stroke(col,100);
- 		ellipse(myX, myY, scal, scal);
+ 		// fill(col,20);
+ 		// stroke(col,100);
+ 		if (blah == 0)
+ 		{
+ 			fill(col,20);
+ 			stroke(col,100);
+ 			rect(myX, myY, scal, scal);	
+ 		}
+ 		if (blah == 1)
+ 		{
+ 			fill(255,255,0,20);
+ 			stroke(255,255,0,100);
+ 			ellipse(myX, myY, scal, scal);
+ 		}
+ 		// if (blah == 2)
+ 		// {
+ 		// 	triangle(myX, myY,myX+scal/2, myY+scal, myX-scal, myY+scal/2);
+ 		// }
+ 		// if (blah == 3)
+ 		// {
+ 		// 	fill(0,0,0,20);
+ 		// }
 
  	}
  }
